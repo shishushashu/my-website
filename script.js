5df74fa80b1ba13965883b692214474f15d6a00a
shishushashu <diodatiluna@gmail.com>
2026-03-03 20:27:09 +0000

    added a button

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const immagini = [
-    'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png',
-    'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png',
-    'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png'
-]
 
-function randomizza() {
 
-    const elemento = document.getElementById('foto')
-    const unica = Math.floor(Math.random() * immagini.length)
-    elemento.src = immagini[unica]
-}
+const bottone = document.getElementById('bottoncino')
 
-window.onload = randomizza;
+bottone.addEventListener('click', function(){
+    console.log('bottone funzionante');
+    
+    
+})
+
+
