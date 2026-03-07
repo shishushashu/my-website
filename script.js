4484d75e4ce7d33a354a8c8545e8e9cbe64ef3d7
shishushashu <diodatiluna@gmail.com>
2026-03-07 10:32:09 +0000

    made a counter

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 
 
-const bottone = document.getElementById('bottoncino')
+let prova = 0
 
-bottone.addEventListener('click', function(){
-    console.log('bottone funzionante');
-    
-    
-})
+let aggiungi = document.querySelector('#aggiungi');
+let togli = document.querySelector('#togli');
+
+
+aggiungi.addEventListener("click", function () {
+   
+    let contatore = document.querySelector('#output');
+    let result = Number(contatore.innerText) + 1;
+    contatore.innerText = result
+
+
+});
+
+togli.addEventListener("click", function () {
+   
+    let contatore = document.querySelector('#output');
+    let result = Number(contatore.innerText) - 1;
+    if (result < 0) {result = 0;}
+    contatore.innerText = result
+
+
+
+});
+
+
+
+
+
 
 
